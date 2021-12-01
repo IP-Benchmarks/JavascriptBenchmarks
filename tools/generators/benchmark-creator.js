2d0149fe3848b6859bb5c3260d6603b58d6bde50
Iulian Preda <iulian.octavian.preda@gmail.com>
2021-12-01 20:06:55 +0100

    Improved generator

--- tools/generators/benchmark-creator.js
+++ tools/generators/benchmark-creator.js
@@ -1,54 +1,61 @@
+const fs = require('fs');
+
 main();
 
 async function main() {
-    const fs = require('fs');
-    const basePath = './libs/benchmarks/src';
+    const basePath = `./libs/benchmarks/src`;
 
     let myArgs = process.argv.slice(2);
     if (myArgs.length < 2) {
         myArgs = await getUserInput();
     }
+    myArgs = myArgs.map((arg) => arg.toLocaleLowerCase());
     const [category, benchmark] = myArgs;
-    createBenchmarkFiles(fs, basePath, category, benchmark);
-    updateIndex(fs, basePath, benchmark, category);
+    createBenchmarkFiles(basePath, category, benchmark);
+    updateIndex(basePath, benchmark, category);
 
     process.exit(0);
 }
 
-function updateIndex(fs, basePath, benchmark, category) {
+function updateIndex(basePath, benchmark, category) {
     const indexData = fs.readFileSync(`${basePath}/index.ts`, 'utf8');
     const benchmarkData = `
-    export * as ${dashToCamelCase(benchmark)} from './lib/${category}/${benchmark}/${benchmark}';`;
+export * as ${dashToCamelCase(benchmark)} from './lib/${category}/${benchmark}/${benchmark}';`;
 
-    fs.writeFileSync(`${basePath}/index.ts`, indexData + benchmarkData, 'utf8');
+    writeFileSync(basePath, 'index.ts', indexData + benchmarkData);
 }
 
-function createBenchmarkFiles(fs, basePath, category, benchmark) {
+function createBenchmarkFiles(basePath, category, benchmark) {
     const testData = `
-    describe('${benchmark}.spec', () => {
-        test('', () => {
-           
-        });
+describe('${benchmark}.spec', () => {
+    test('', () => {
+        
     });
+});
     `;
 
     const benchmarkData = `
-    import { generateMixedPrimitivesArray, generateSamples, IBenchmark, runBenchmark } from '@javascript-benchmarks/shared';
+import { generateMixedPrimitivesArray, generateSamples, IBenchmark, runBenchmark } from '@javascript-benchmarks/shared';
 
-    export function test(): Array<IBenchmark> {
+export function test(): Array<IBenchmark> {
     const matrix = generateSamples(1000, () => generateMixedPrimitivesArray(1000, true));
 
     return [
-        runBenchmark('Benchmark 1', () => undefined, matrix),
-        runBenchmark('Benchmark 2', () => undefined, matrix),
+        runBenchmark('Benchmark 1', () => 'Please update this function', matrix),
+        runBenchmark('Benchmark 2', () => 'Please update this function', matrix),
     ];
 }
     `;
 
-    fs.writeFileSync(`${basePath}/lib/${category}/${benchmark}/${benchmark}.ts`, benchmarkData, 'utf8');
-    fs.writeFileSync(`${basePath}/lib/${category}/${benchmark}/${benchmark}.spec.ts`, testData, 'utf8');
+    writeFileSync(`${basePath}/lib/${category}/${benchmark}`, `${benchmark}.ts`, benchmarkData);
+    writeFileSync(`${basePath}/lib/${category}/${benchmark}`, `${benchmark}.spec.ts`, testData);
 
     console.log(`Benchmark ${benchmark} created in ${category} category`);
+}
+
+function writeFileSync(folderPath, fileName, content) {
+    if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath, { recursive: true });
+    fs.writeFileSync(`${folderPath}/${fileName}`, content, 'utf8');
 }
 
 async function getUserInput() {
