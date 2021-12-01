44ca68271a7d27ecadcd3e9e8b25480c3dd5c428
Iulian Preda <iulian.octavian.preda@gmail.com>
2021-12-01 19:53:14 +0100

    Update main.ts

--- apps/runner/src/main.ts
+++ apps/runner/src/main.ts
@@ -1,33 +1,28 @@
-import { promises as fs } from 'fs';
+import * as jsBenchmarks from '@javascript-benchmarks/benchmarks';
+import { dashToCamelCase, IBenchmark } from '@javascript-benchmarks/shared';
 
 const myArgs = process.argv.slice(2);
 
 myArgs.forEach(async (element) => {
-    console.log(element);
-    const contentAsTs = await fs.readFile(element, 'utf8');
-
-    element.replace('src', 'dist');
-    const contentAsJs = await fs.readFile(element, 'utf8');
-
-    const moduleToBenchmark = await import(element);
-
-    const testingFunction = moduleToBenchmark.test as (callback: (...args: unknown[]) => unknown) => () => unknown;
-    if (typeof testingFunction === 'function') {
-        Object.entries(moduleToBenchmark).forEach(([key, value]) => {
-            console.log(key);
-            console.log(value);
-
-            // const nameTimeArr = [];
-            if (typeof value === 'function') {
-                const fct = value as (...args: unknown[]) => unknown;
-                // const benchmarkFunction = testingFunction(fct);
-                // const time = measureTime(benchmarkFunction);
-                // nameTimeArr.push([camelCaseToSentence(key), time]);
-            }
-        });
-    }
-
-    console.log(contentAsTs, contentAsJs);
+    await runBenchmark(element);
+    // console.log(element);
+    // const contentAsTs = await fs.readFile(element, 'utf8');
+    // element.replace('src', 'dist');
+    // const contentAsJs = await fs.readFile(element, 'utf8');
+    // const moduleToBenchmark = await import('@javascript-benchmarks/benchmarks').then((x) => x.importBenchmark(element));
+    // const moduleToBenchmark = await import(/* webpackMode: "eager" */ element);
+    // console.log(contentAsTs, contentAsJs);
 });
+
+export async function runBenchmark(path: string) {
+    const moduleName = dashToCamelCase(path.split('/').pop().replace('.ts', ''));
+    const moduleToBenchmark = jsBenchmarks[moduleName];
+
+    if (typeof moduleToBenchmark.test === 'function') {
+        const testingFunction = moduleToBenchmark.test as () => Array<IBenchmark>;
+        console.log(`Running benchmark ${path}`, testingFunction());
+        return testingFunction();
+    }
+}
 
 export {};
